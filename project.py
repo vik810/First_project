69271698c9b563f8aeb1f59b75fd82ce0b21ef78
vik810 <56610139+vik810@users.noreply.github.com>
2019-11-27 15:51:52 +0200

    hj

---
+++ project.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+fig, ax = plt.subplots()
+
+dot, = plt.plot([], [], 'o', color='y')
+
+xdata = []
+ydata = []
+
+ax.set_ylim(-10, 10)
+ax.set_xlim(-10, 50)
+
+R = 10
+
+def iskriv_ray(m, yt, alpha)
+    r=yt*np.tan(alpha)
+    
+    xdata.append(R*np.cos(t)**3)
+    ydata.append(R*np.sin(t)**3)
+    dot.set_data(xdata, ydata)
+    return dot
+
+anim = FuncAnimation(fig, 
+                    cicloid,
+                    frames=np.arange(0, 8*np.pi, 0.1),
+                    interval=50)
+anim.save('anim.gif')
